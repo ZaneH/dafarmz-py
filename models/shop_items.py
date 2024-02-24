ba405736b9a63f8254d7a44bd98fe3c8a617495a
Zane Helton <me@zane.pw>
2024-02-24 12:19:25 -0500

    docs: improve comments in ShopItemModel.py

--- models/shop_items.py
+++ models/shop_items.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from bson import ObjectId
 from pydantic import BaseModel, Field
@@ -17,22 +17,36 @@ class ShopItemModel(BaseModel):
     Represents a singular shop item from the database.
     """
     id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
-    key: str = ""  # The unique key for the item
-    name: str = ""  # The display name of the item
-    description: str = ""  # The description of the item
-    cost: int = 0  # The cost of the item (coins)
-    grow_time_hr: float = 1  # The time it takes to grow per yield
-    resell_price: int = 0  # The price to resell the item
-    yields: Dict[str, YieldModel] = {}  # The yields of the item
-    # The yields of the item when it dies
+    key: str = ""
+    """The unique key for the item"""
+    name: str = ""
+    """The display name of the item"""
+    description: str = ""
+    """The description of the item"""
+    cost: int = 0
+    """The cost of the item (coins)"""
+    grow_time_hr: float = 1
+    """The time it takes to grow per yield"""
+    resell_price: int = 0
+    """The price that the store will buy this item for"""
+    yields: Dict[str, YieldModel] = {}
+    """The yields of the item (may involve probabilities)"""
     death_yields: Dict[str, YieldModel] = {}
-    total_yields: int = 0  # The amount of yields the item can produce
-    level_required: int = 0  # The level required to see this item in the shop
-    # The environment buffs of the item
+    """The yields of the item when it dies (may involve probabilities)"""
+    total_yields: int = 0
+    """The amount of yields the item can produce"""
+    level_required: int = 0
+    """The level required to see this item in the shop"""
     environment_buffs: Dict[Environment, float] = {}
+    """The environment buffs of the item"""
 
     @classmethod
-    async def find_all(cls):
+    async def find_all(cls) -> list["ShopItemModel"]:
+        """
+        Returns all the items in the shop.
+
+        :return: A list of all the shop items.
+        """
         collection = Database.get_instance().get_collection(COLLECTION_NAME)
         cursor = collection.find({})
         items = await cursor.to_list(length=None)
@@ -41,7 +55,13 @@ class ShopItemModel(BaseModel):
         return items
 
     @classmethod
-    async def find_buyable(cls):
+    async def find_buyable(cls) -> list["ShopItemModel"]:
+        """
+        Returns all the items that can be bought from the shop. Items that
+        have a cost of 0 or None are not buyable.
+
+        :return: A list of buyable shop items.
+        """
         collection = Database.get_instance().get_collection(COLLECTION_NAME)
         cursor = collection.find({
             "cost": {"$gt": 0}
@@ -51,7 +71,12 @@ class ShopItemModel(BaseModel):
 
         return items
 
-    def get_ripe_image_path(self):
+    def get_ripe_image_path(self) -> Optional[str]:
+        """
+        Returns the path to the ripe image of the item.
+
+        :return: The path to the ripe image of the item.
+        """
         ripe_image_name = get_ripe_image(self.key)
         if not ripe_image_name:
             return None
